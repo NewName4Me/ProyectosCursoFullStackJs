fcc09baa483cc21e7b13ee8ebe6a66eb2e9dd99e
NewName4Me <tortarod@gmail.com>
2024-06-19 13:01:00 +0200

    comienzo proyecto mostrar clima Fetch APi

---
+++ P10_BuscadorClima/js/app.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('.container');
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+
+window.addEventListener('load',()=>{
+    
+});
