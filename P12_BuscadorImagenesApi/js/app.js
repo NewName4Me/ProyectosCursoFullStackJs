a4275bb531e4700735b05d8b25c286e4d55e06ec
NewName4Me <tortarod@gmail.com>
2024-06-25 22:41:36 +0200

    mostrar imagenes y ajustar per_page basicamente per page si miras la documentacion de la api es un valor que toma 20 predeterminado y este indica el numero de imagenes que se muestran por ventana, pero se puede modificar en la url como veras

--- P12_BuscadorImagenesApi/js/app.js
+++ P12_BuscadorImagenesApi/js/app.js
@@ -1,7 +1,6 @@
 //region Variables
 const resultado = document.querySelector('#resultado');
 const formulario = document.querySelector('#formulario');
-const API_KEY = '44561483-815f678ecc162bb56735260a9';
 
 //region DOM Loaded
 document.addEventListener('DOMContentLoaded', () => {
@@ -17,6 +16,8 @@ function validarFormulario(e) {
         mostrarAlerta('Rellene el campo');
         return;
     }
+
+    buscarImagenes(terminoBusqueda);
 }
 
 //region Show Alerta
@@ -34,4 +35,47 @@ function mostrarAlerta(mensaje) {
     setTimeout(() => {
         alerta.remove();
     }, 2500);
+}
+
+//region Search Images
+function buscarImagenes(termino) {
+    const API_KEY = '44561483-815f678ecc162bb56735260a9';
+    const url = `https://pixabay.com/api/?key=${API_KEY}&q=${termino}&per_page=100`;
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => mostrarImagenes(resultado.hits));
+}
+
+//region Show Images
+function mostrarImagenes(imagenes) {
+    limpiarHTML(resultado);
+    //iterar sobre lista de imagenes y construir html
+    imagenes.forEach(imagen => {
+        const { previewURL, likes, views, largeImageURL } = imagen;
+
+        resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${previewURL}">
+                    <div class="p-4">
+                        <p class="font-bold">${likes} <span class="font-light">Me gusta</span></p>
+                        <p class="font-bold">${views} <span class="font-light">Veces vista</span></p>
+
+                        <a class="w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"
+                        href="${largeImageURL}" target="_blanck" rel="noopener noreferrer">
+                            Ver imagen
+                        </a>
+                    </div>
+                <div>        
+            </div>
+        `;
+    });
+}
+
+//region Limpiar HTML
+function limpiarHTML(objeto) {
+    while (objeto.firstChild) {
+        objeto.removeChild(objeto.firstChild);
+    }
 }
