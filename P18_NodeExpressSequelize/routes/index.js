69769943a3b754c84cbfb963364c81aa6f38b633
NewName4Me <tortarod@gmail.com>
2024-07-12 11:42:05 +0200

    añadir el resto de vistas y titulo de forma dinamica

--- P18_NodeExpressSequelize/routes/index.js
+++ P18_NodeExpressSequelize/routes/index.js
@@ -4,13 +4,24 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => { /* req => lo que envviamos res => la respuesta */
-    res.render('inicio');
+    res.render('inicio', {
+        pagina: 'Inicio'
+    });
 });
 router.get('/nosotros', (req, res) => {
-
-    const viajes = 'Viaje a Alemania';
-
-    res.render('nosotros');
+    res.render('nosotros', {
+        pagina: 'Nosotros'
+    });
+});
+router.get('/viajes', (req, res) => {
+    res.render('viajes', {
+        pagina: 'Viajes'
+    });
+});
+router.get('/testimoniales', (req, res) => {
+    res.render('testimoniales', {
+        pagina: 'Testimoniales'
+    });
 });
 
 export default router;
