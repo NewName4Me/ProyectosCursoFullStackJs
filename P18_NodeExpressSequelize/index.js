a4dfec5642b6d017a72bdc16002a4c1ae99fa512
NewName4Me <tortarod@gmail.com>
2024-07-12 11:34:38 +0200

    aprender middleware utilizando locals pasar año actual al footer

--- P18_NodeExpressSequelize/index.js
+++ P18_NodeExpressSequelize/index.js
@@ -10,6 +10,15 @@ const port = process.env.PORT || 4000;
 /* habilitar PUG */
 app.set('view engine', 'pug');
 
+//crear nuestro propio middleware
+//obtener el año actual
+app.use((req, res, next) => {
+    const year = new Date();
+    res.locals.ActualYear = year.getFullYear();
+
+    return next();
+});
+
 //definir la carpeta publica
 app.use(express.static('public'));
 
