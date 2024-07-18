a42eaef6d3d230710972eea61ee2cba23ad1cd5c
NewName4Me <tortarod@gmail.com>
2024-07-18 11:19:52 +0200

    añadir valores de nuestro rango con step

--- P19_CotizadorReact/cotizador prestamos/src/App.jsx
+++ P19_CotizadorReact/cotizador prestamos/src/App.jsx
@@ -3,22 +3,36 @@ import { useState } from 'react';
 
 function App() {
 
+  /* state es para valores que se van a modificar */
   const [cantidad, setCantidad] = useState(10000);
+
+  const MIN = 0;
+  const MAX = 20000;
+  const STEP = 100;
 
   //funcion encargada de manejar el valor de nuestro range
   function handleChange(e) {
-    console.log(e.target.value);
+    setCantidad(e.target.value);
   }
 
   return (
     <>
       <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
         <Header />
+
         <input
           type="range"
           className="w-full h-6 bg-gray-600 accent-lime-500 hover:accent-lime-600"
           onChange={handleChange}
+          min={MIN}
+          max={MAX}
+          step={STEP}
+          value={cantidad} /* asi indicamos su posicion inical(que coincide en el centro)*/
         />
+
+        <p className="text-center my-10 text-5xl font-extrabold text-indigo-500">
+          {cantidad}
+        </p>
       </div>
     </>
   )
