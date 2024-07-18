9985f6eaf156a634b9b25a20b1841ddd226a6a7f
NewName4Me <tortarod@gmail.com>
2024-07-18 12:04:05 +0200

    añadir plazos y estructurar codigo usar herlpers/utils para formatear dinero

--- P19_CotizadorReact/cotizador prestamos/src/App.jsx
+++ P19_CotizadorReact/cotizador prestamos/src/App.jsx
@@ -1,4 +1,6 @@
 import Header from "./components/Header";
+import Button from "./components/Button";
+import { formatearDinero } from "./helpers/index.js";
 import { useState } from 'react';
 
 function App() {
@@ -15,10 +17,28 @@ function App() {
     setCantidad(e.target.value);
   }
 
+  function handlerClickDecremento() {
+    const valor = cantidad - STEP;
+    if (valor < MIN) { alert('Cantidad No Válida'); return; }
+    setCantidad(valor);
+  }
+
+  function handlerClickIncremento() {
+    const valor = cantidad + STEP;
+    if (valor > MAX) { alert('Cantidad No Válida'); return; }
+    setCantidad(valor);
+  }
+
+
   return (
     <>
       <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
         <Header />
+
+        <div className="flex justify-between my-6">
+          <Button operador='-' fn={handlerClickDecremento} />
+          <Button operador='+' fn={handlerClickIncremento} />
+        </div>
 
         <input
           type="range"
@@ -31,8 +51,22 @@ function App() {
         />
 
         <p className="text-center my-10 text-5xl font-extrabold text-indigo-500">
-          {cantidad}
+          {formatearDinero(cantidad)}
         </p>
+
+        <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+          Elige un <span className="text-indigo-500">plazo</span> a pagar
+        </h2>
+
+        <select 
+        name="" id=""
+        className="mt-5 w-full bg-white border-gray-300 rounded-lg text-center text-xl 
+        font-bold text-gray-500" 
+        >
+          <option value="6">6 meses</option>
+          <option value="12">12 meses</option>
+          <option value="24">24 meses</option>
+        </select>
       </div>
     </>
   )
