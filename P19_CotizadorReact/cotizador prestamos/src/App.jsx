4bd10f383f6d955bf8bbb4066fd7d1cb3e1961ca
NewName4Me <tortarod@gmail.com>
2024-07-18 11:01:10 +0200

    asociar input a funcion handle

--- P19_CotizadorReact/cotizador prestamos/src/App.jsx
+++ P19_CotizadorReact/cotizador prestamos/src/App.jsx
@@ -1,8 +1,25 @@
+import Header from "./components/Header";
+import { useState } from 'react';
+
 function App() {
+
+  const [cantidad, setCantidad] = useState(10000);
+
+  //funcion encargada de manejar el valor de nuestro range
+  function handleChange(e) {
+    console.log(e.target.value);
+  }
 
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+        <Header />
+        <input
+          type="range"
+          className="w-full h-6 bg-gray-600 accent-lime-500 hover:accent-lime-600"
+          onChange={handleChange}
+        />
+      </div>
     </>
   )
 }
---
+++ P19_CotizadorReact/cotizador prestamos/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <h1 className="font-extrabold text-gray-500 text-center">
+            ¿Cuánto <span className="text-indigo-500">dinero</span> necesitas?
+        </h1>
+    )
+}
+
+export default Header;
